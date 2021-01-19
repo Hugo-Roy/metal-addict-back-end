166eb056610bdf865d2b271c41cb92b8d4db82d6
Hugo Roy <roy.hugo21@mail.com>
2021-01-19 14:41:40 +0100

    Add Profiler Pack

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -6,19 +6,20 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ReviewController extends AbstractController
 {
     /**
      * @Route("/api/review", name="review_list")
      */
-    public function list(Request $request): Response
+    public function list(Request $request, SerializerInterface $serializer): Response
     {
-        $request->getContent();
+        $jsonContent = $request->getContent();
+
+        $content = json_decode($jsonContent);
         
-        return $this->json([
-            "test ok"
-        ]);
+        return $this->json($content);
     }
 
     // [
