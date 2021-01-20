9897abd1e766af49418afcd737fa7f7149afdb94
Hugo Roy <roy.hugo21@mail.com>
2021-01-20 11:06:44 +0100

    Add Methods country_list and country_search

--- src/Controller/CountryController.php
+++ src/Controller/CountryController.php
@@ -2,19 +2,41 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Country;
+use App\Repository\CountryRepository;
+use App\Service\SetlistApi;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CountryController extends AbstractController
 {
-    /**
-     * @Route("/country", name="country")
+     /**
+     * @Route("/api/country", name="country_list", methods="GET")
      */
-    public function index(): Response
+    public function list(CountryRepository $countryRepository): Response
     {
-        return $this->render('country/index.html.twig', [
-            'controller_name' => 'CountryController',
-        ]);
+        $countries = $countryRepository->findAll();
+        
+        return $this->json($countries, Response::HTTP_OK, [], ['groups' => 'country_get']);
+    }
+
+    /**
+     * @Route("/api/country/{id}", name="country")
+     */
+    public function searchCountry(Country $country, SetlistApi $setlistApi, Request $request)
+    {
+        $jsonContent = $request->getContent();
+
+        $researchParams = json_decode($jsonContent, true);
+
+        // $researchParams['parameters']['CountryName'] = $country->getName();
+
+        $researchParams['parameters']['countryCode'] = $country->getCountryCode();
+        
+        $responseContent = $setlistApi->fetchEventsList($researchParams);
+
+        return $this->json($responseContent);
     }
 }
