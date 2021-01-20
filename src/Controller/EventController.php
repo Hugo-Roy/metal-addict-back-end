a1de7cb9d6444020ee8e3f845d325ad17c3e115c
Hug0-Drelon <hugo.drelon@gmail.com>
2021-01-20 16:15:53 +0100

    modify country parameter in request

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Band;
+use App\Repository\CountryRepository;
 use App\Service\SetlistApi;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,13 +14,22 @@ class EventController extends AbstractController
     /**
      * @Route("/api/search/{id}", name="event_search", methods="GET")
      */
-    public function search(Band $band, SetlistApi $setlistApi, Request $request)
+    public function search(Band $band, SetlistApi $setlistApi, Request $request, CountryRepository $countryRepository)
     {
         $jsonContent = $request->getContent();
 
         $researchParams = json_decode($jsonContent, true);
 
         $researchParams['parameters']['artistName'] = $band->getName();
+
+        if(isset($researchParams['parameters']['countryId'])) {
+            $country = $countryRepository->find($researchParams['parameters']['countryId']);
+    
+            $researchParams['parameters']['countryCode'] = $country->getCountryCode();
+
+            unset($researchParams['parameters']['countryId']);
+        }
+        
 
         $responseContent = $setlistApi->fetchEventsList($researchParams);
 
