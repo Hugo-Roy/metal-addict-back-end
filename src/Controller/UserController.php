84b008dd31a836592b8f05de36315000ae07fa94
Hugo Roy <roy.hugo21@mail.com>
2021-01-21 16:50:00 +0100

    Done user endpoint

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,12 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
     /**
-     * @Route("/api/users", name="api_users", methods="GET")
+     * @Route("/api/users", name="api_users", methods={"GET"})
      */
     public function listAll(UserRepository $userRepository): Response
     {
-        $users = $userRepository->findAll();
+        // $users = $userRepository->findAll();
         
-        return $this->json($users, Response::HTTP_OK, [], ["groups" => "user_get"]);
+        return $this->json($userRepository->findAll(), Response::HTTP_OK, [], ["groups" => "user_get"]);
     }
 }
