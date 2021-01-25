0d203fa3d40edf0d098ab27cf239c5f6b53d3963
Hugo Roy <roy.hugo21@gmail.com>
2021-01-25 14:32:47 +0100

    add the review_put/patch method

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -53,6 +54,25 @@ class ReviewController extends AbstractController
         $em->flush();
 
         return $this->json($review, Response::HTTP_CREATED, ['Location' => $this->generateUrl('review_show', ['id' => $review->getId()])], ['groups' => 'review_get']);
+    }
+
+    /**
+     * @Route("/api/review/{id<\d+>}", name="review_update", methods={"PUT", "PATCH"})
+     */
+    public function update(Review $review, EntityManagerInterface $em, SerializerInterface $serializer, Request $request)
+    {
+        if($review === null)
+        {
+            return $this->json(['error' => "Review non trouvée."], Response::HTTP_NOT_FOUND);
+        }
+
+        $jsonContent = $request->getContent();
+
+        $serializer->deserialize($jsonContent, Review::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $review]);
+
+        $em->flush();
+
+        return $this->json(['message' => 'Review modifiée.'], Response::HTTP_OK);
     }
 }
 
