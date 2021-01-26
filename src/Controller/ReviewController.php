9582a6f922780ccfd1978ad8007e237245e7d2ed
Hug0-Drelon <hugo.drelon@gmail.com>
2021-01-26 10:04:45 +0100

    remove uses

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -4,19 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Event;
 use App\Entity\Review;
-use App\Entity\User;
 use App\Repository\ReviewRepository;
-use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ReviewController extends AbstractController
 {
