69edb55fea129477ee0abab22d06492bd2efe1a7
Hug0-Drelon <hugo.drelon@gmail.com>
2021-01-27 00:55:56 +0100

    handle offset

--- src/Command/GetMusicbrainzBands.php
+++ src/Command/GetMusicbrainzBands.php
@@ -2,6 +2,8 @@
 
 namespace App\Command;
 
+use App\Entity\Band;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -13,9 +15,13 @@ class GetMusicbrainzBands extends Command
 
     private $client;
 
-    public function __construct(HttpClientInterface $client)
+    private $entityManager;
+
+    public function __construct(HttpClientInterface $client, EntityManagerInterface $entityManager)
     {
         $this->client = $client;
+
+        $this->entityManager = $entityManager;
 
         parent::__construct();
     }
@@ -28,9 +34,28 @@ class GetMusicbrainzBands extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $isBands = false;
+        $offset = 0;
+
+        while ($isBands === false) {
+            $isBands = $this->fetchFromMusicbrainz($offset, $output);
+            $offset += 100;
+        };
+
+        // return this if there was no problem running the command
+        // (it's equivalent to returning int(0))
+        return Command::SUCCESS;
+
+        // or return this if some error happened during the execution
+        // (it's equivalent to returning int(1))
+        // return Command::FAILURE;
+    }
+
+    private function fetchFromMusicbrainz($offset, $output)
+    {
         $response = $this->client->request(
             'GET',
-            'https://musicbrainz.org/ws/2/artist?query=tag:metal&offset=0',
+            'https://musicbrainz.org/ws/2/artist?query=tag:metal&limit=100&offset='.$offset,
             [
                 'headers' => [
                     'Accept' =>'application/json',
@@ -44,15 +69,14 @@ class GetMusicbrainzBands extends Command
         foreach ($bands['artists'] as $band) {
             $output->writeln($band['id']);
             $output->writeln($band['name']);
+            $bandEntity = new Band();
+            $bandEntity->setMusicbrainzId($band['id']);
+            $bandEntity->setName($band['name']);
+            $this->entityManager->persist($bandEntity);
         }
 
+        $this->entityManager->flush();
 
-        // return this if there was no problem running the command
-        // (it's equivalent to returning int(0))
-        return Command::SUCCESS;
-
-        // or return this if some error happened during the execution
-        // (it's equivalent to returning int(1))
-        // return Command::FAILURE;
+        return empty($bands['artists']);
     }
 }
