9ee69503b0ba7504923f275cfbcc9887160446d5
Hug0-Drelon <hugo.drelon@gmail.com>
2021-02-04 20:14:58 +0100

    change command name

--- src/Command/GetCountries.php
+++ src/Command/GetCountries.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class GetCountries extends Command {
-    protected static $defaultName = 'app:get:bands';
+    protected static $defaultName = 'app:get:countries';
 
     private $setlistApi;
 
