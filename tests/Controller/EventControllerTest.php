50ac29141a54197d043818a56b2a162418716dd3
Hug0-Drelon <hugo.drelon@gmail.com>
2021-02-13 17:35:39 +0100

    create show event test

--- tests/Controller/EventControllerTest.php
+++ tests/Controller/EventControllerTest.php
@@ -9,7 +9,7 @@ class EventControllerTest extends WebTestCase
     public function testSearchEvent()
     {
         $client = static::createClient();
-
+        
         $queryParameters = [
             'cityName' => 'Arras',
             'venueName' => 'Grand-Place',
@@ -17,13 +17,26 @@ class EventControllerTest extends WebTestCase
             'year' => '2008',
             'p' => '1',
         ];
-
+        
         $client->xmlHttpRequest('GET', '/api/search/1?cityName=Arras&venueName=Grand-Place&countryId=76&year=2008&p=1');
+        
+        $response = $client->getResponse();
+        
+        $this->assertJson($response->getContent());
+        
+        $this->assertResponseIsSuccessful();
+    }
+    
+    public function testShowEvent()
+    {
+        $client = static::createClient();
+        
+        $client->xmlHttpRequest('GET', '/api/event/3bea6ccc');
 
         $response = $client->getResponse();
-
+        
         $this->assertJson($response->getContent());
-
+        
         $this->assertResponseIsSuccessful();
     }
 }
