4403b1af18e580054a0a242263bfc2b41a742da8
Hug0-Drelon <hugo.drelon@gmail.com>
2021-02-13 17:24:44 +0100

    create event search test

---
+++ tests/Controller/EventControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class EventControllerTest extends WebTestCase
+{
+    public function testSearchEvent()
+    {
+        $client = static::createClient();
+
+        $queryParameters = [
+            'cityName' => 'Arras',
+            'venueName' => 'Grand-Place',
+            'CountryId' => '76',
+            'year' => '2008',
+            'p' => '1',
+        ];
+
+        $client->xmlHttpRequest('GET', '/api/search/1?cityName=Arras&venueName=Grand-Place&countryId=76&year=2008&p=1');
+
+        $response = $client->getResponse();
+
+        $this->assertJson($response->getContent());
+
+        $this->assertResponseIsSuccessful();
+    }
+}
