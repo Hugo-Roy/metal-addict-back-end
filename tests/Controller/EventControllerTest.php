4b5d943e8f13e07be41818195e88bb8b2fde67b8
Hug0-Drelon <hugo.drelon@gmail.com>
2021-02-13 17:36:43 +0100

    move all query parameters to an array for event search test

--- tests/Controller/EventControllerTest.php
+++ tests/Controller/EventControllerTest.php
@@ -18,7 +18,7 @@ class EventControllerTest extends WebTestCase
             'p' => '1',
         ];
         
-        $client->xmlHttpRequest('GET', '/api/search/1?cityName=Arras&venueName=Grand-Place&countryId=76&year=2008&p=1');
+        $client->xmlHttpRequest('GET', '/api/search/1', $queryParameters);
         
         $response = $client->getResponse();
         
