03b1a08eae58a4beee154114c017596800dd8e4a
Hug0-Drelon <hugo.drelon@gmail.com>
2021-02-13 20:21:45 +0100

    test add avatar authorization

--- tests/Controller/AnonymousTest.php
+++ tests/Controller/AnonymousTest.php
@@ -42,6 +42,7 @@ class AnonymousTest extends WebTestCase
         $types = [
             'picture',
             'review',
+            'user/avatar',
         ];
 
         $setlistId = '6b876a6e';
@@ -59,6 +60,65 @@ class AnonymousTest extends WebTestCase
         $client = self::createClient();
 
         $client->xmlHttpRequest('DELETE', $url);
+
+        $response = $client->getResponse();
+
+        $this->assertJson($response->getContent());
+        
+        $this->assertResponseStatusCodeSame(401);
+    }
+
+    public function putAndPatchUrlProvider()
+    {
+        $types = [
+            'review',
+            'user',
+        ];
+
+        $setlistId = '6b876a6e';
+
+        foreach ($types as $type) {
+            yield ['/api/' . $type . '/1'];
+        }
+    }
+
+    /**
+     * @dataProvider putAndPatchUrlProvider
+     */
+    public function testPatchAuthorization($url)
+    {
+        $client = self::createClient();
+
+        $client->xmlHttpRequest('PATCH', $url);
+
+        $response = $client->getResponse();
+
+        $this->assertJson($response->getContent());
+        
+        $this->assertResponseStatusCodeSame(401);
+    }
+
+    /**
+     * @dataProvider putAndPatchUrlProvider
+     */
+    public function testPutAuthorization($url)
+    {
+        $client = self::createClient();
+
+        $client->xmlHttpRequest('PUT', $url);
+
+        $response = $client->getResponse();
+
+        $this->assertJson($response->getContent());
+        
+        $this->assertResponseStatusCodeSame(401);
+    }
+
+    public function testAddAvatar()
+    {
+        $client = self::createClient();
+
+        $client->xmlHttpRequest('POST', '/api/user/avatar/1');
 
         $response = $client->getResponse();
 
